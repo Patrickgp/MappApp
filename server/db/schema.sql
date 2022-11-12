199df10477ab96511ee7fbec49b0a6bf7bdab444
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-11-12 15:51:12 -0500

    connected MySQL server-side and React client-side

---
+++ server/db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS mappapp_db;
+CREATE DATABASE mappapp_db;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,30 @@
 const express = require("express");
+const sequelize = require("./config/connection");
+const session = require("express-session");
 const path = require("path");
-const cors = require("cors");
+const routes = require("./controllers");
 
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3001;
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
+app.use(session(sess));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({}));
-app.use(cors());
+app.use(routes);
 
 // // Serve up static assets
 if (process.env.NODE_ENV === "production") {
@@ -19,4 +33,8 @@ if (process.env.NODE_ENV === "production") {
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now Listening"));
 });
